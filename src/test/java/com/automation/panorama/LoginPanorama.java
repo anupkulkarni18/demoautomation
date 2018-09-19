14cb3194026543ed22cfe67dafc179606bf70a54
rohitl <rohitl@ULTP_434.synerzipune.local>
2018-09-19 21:52:48 +0530

    initial commit

--- src/test/java/com/automation/panorama/LoginPanorama.java
+++ src/test/java/com/automation/panorama/LoginPanorama.java
@@ -2,8 +2,13 @@ package com.automation.panorama;
 
 import org.testng.annotations.Test;
 
+import com.automation.base.CustomException;
+import com.automation.browser.Browser;
+
 public class LoginPanorama {
-  @Test
-  public void f() {
-  }
+		@Test
+		public void beforeTest() throws CustomException {
+	Browser.initializeBrowser();
+		}
+
 }
